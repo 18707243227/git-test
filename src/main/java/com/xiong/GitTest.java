a7eafe4fff74eb2601ce7cec75fa0f5c38e2e7c3
xzy <2739427069@qq.com>
2023-10-26 19:47:04 +0800

    second commit

--- src/main/java/com/xiong/GitTest.java
+++ src/main/java/com/xiong/GitTest.java
@@ -8,6 +8,6 @@ package com.xiong;/*
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git-----");
+        System.out.println("hello 222222");
     }
 }
