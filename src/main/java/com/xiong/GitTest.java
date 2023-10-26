d4c407088f0d388ee925192ea48f306a0bfdb394
xzy <2739427069@qq.com>
2023-10-26 19:45:42 +0800

    first commit

---
+++ src/main/java/com/xiong/GitTest.java
@@ -0,0 +1,13 @@
+package com.xiong;/*
+ * @title GitTest
+ * @description
+ * @author 小熊
+ * @version 1.0.0
+ * @create 2023/10/26 19:42
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git-----");
+    }
+}
